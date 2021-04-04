adf6afba970f1623077c3aafb5bb15ac811e9142
Saad Aslam <saadusufzai@gmail.com>
2021-04-04 21:47:16 +0500

    Aws lambda created

--- lib/aws-stack.ts
+++ lib/aws-stack.ts
@@ -1,5 +1,7 @@
 import * as cdk from '@aws-cdk/core';
 import * as s3 from '@aws-cdk/aws-s3'
+import * as lambda from "@aws-cdk/aws-lambda";
+import * as apigw from "@aws-cdk/aws-apigateway";
 
 export class AwsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -8,5 +10,16 @@ export class AwsStack extends cdk.Stack {
     new s3.Bucket(this,'FirstS3Bucket',{
       versioned:true
     })
+
+    const hello = new lambda.Function(this, "HelloHandlerFunction",{
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code: lambda.Code.fromAsset("lambda"),
+      handler:"index.handler",
+    });
+
+    new apigw.LambdaRestApi(this, "EndPoint", {
+      handler: hello,
+    })
+
   }
 }
